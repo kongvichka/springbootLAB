9ca3dac5447fb9a98bf6e79d17a5742150eb3964
Kong Vichka <118750886+kongvichka@users.noreply.github.com>
2025-01-16 00:12:25 +0700

    Delete JavaPro directory

--- JavaPro/productservice/productservice/src/main/java/com/example/productservice/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.productservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- JavaPro/userservice/userservice/src/main/java/com/example/userservice/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.userservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
